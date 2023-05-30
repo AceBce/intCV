4aefae5d61f7a2ce50115c57d069710710c27f80
AceBce <1062530354@qq.com>
2023-05-30 11:28:11 +0800

    开始进行依赖分析

--- tools/llvm-multivariable-find.cpp
+++ tools/llvm-multivariable-find.cpp
@@ -36,6 +36,7 @@ using llvm::errs;
 class GlobalVar {
     string name;//变量名
     int type; //type = 1是普通变量, type = 2是数组变量， type = 3是结构体变量
+    string basename;//如果是数组或是结构体的话就有basename，eg: f[3]的basename为f
     int id;
     set<string> belongtofun;//所属的函数
     set<unsigned int>belongtoline;//所属的行号
@@ -45,11 +46,14 @@ class GlobalVar {
     map<string, int> datadep;//存储与它有数据依赖的全局变量，以及依赖了几次
     map<string, int> controldep;//存储与它有控制依赖的全局变量，以及依赖了几次
     //以上存储的都是变量名
+    set<dg::sdg::DGNode*> nodes;//存储它出现过的节点
   public:
     GlobalVar(const string &name, const string &funname) {GlobalVar::name = name, belongtofun.insert(funname); }
     explicit GlobalVar(const string &name) : name(name) {}
     void setName(const string &name)  {GlobalVar::name = name; }
     const string &getName() {return name; }
+    void setBaseName(const string &name) {basename = name; }
+    string &getBaseName() {return basename;;}
     void setID(int id) {this->id = id; }
     int getID() {return id; }
     void addBelongtofun(string funname) {belongtofun.insert(std::move(funname)); }
@@ -70,6 +74,9 @@ class GlobalVar {
     map<string, int> &getDatadep() {return datadep; }
     void addControldep(const string &that) {controldep[that]++; }
     map<string, int> &getControldep() {return controldep; }
+    void addnode(dg::sdg::DGNode *n) {nodes.insert(n); }
+    set<dg::sdg::DGNode*> getnode() {return nodes; }
+
 };
 struct FunctionInfo {
     string funname;
@@ -77,13 +84,14 @@ struct FunctionInfo {
     vector<pair<string, int>> vars;
 };
 int codedistance = 10;
+map<string, set<string>> basenametoname;
 map<string, GlobalVar*> nametovar;
 set<FunctionInfo*> funinfos;
 set<GlobalVar*> globals;
 set<string> funs;
 set<tuple<int, int, string>> linetofun;//存储一个函数的startline, endline, funname
 map<string, set<GlobalVar*>> funhasglobalvar;//函数里有哪些变量
-
+map<Instruction*, set<GlobalVar*>> Instrtovar;//指令里有哪些变量
 llvm::cl::opt<bool> enable_debug(
         "dbg", llvm::cl::desc("Enable debugging messages (default=false)."),
         llvm::cl::init(false), llvm::cl::cat(SlicingOpts));
@@ -94,6 +102,10 @@ llvm::cl::opt<bool> dump_bb_only(
                        " (default=false)."),
         llvm::cl::init(false), llvm::cl::cat(SlicingOpts));
 bool isStructArray(Type* type);//递归判断是不是结构体数组
+bool isCloseName(const string &name1, const string &name2);
+void Dep(llvmdg::SystemDependenceGraph &sdg, GlobalVar *Gvar);
+void controldep(llvmdg::SystemDependenceGraph &sdg, dg::sdg::DGNode *node);
+void datadep(llvmdg::SystemDependenceGraph &sdg, dg::sdg::DGNode *node);
 int main(int argc, char *argv[]) {
     setupStackTraceOnError(argc, argv);
     SlicerOptions options = parseSlicerOptions(argc, argv);
@@ -174,6 +186,15 @@ int main(int argc, char *argv[]) {
                             Gvar->addBelongtofun(funname);
                             globals.insert(Gvar);
                             nametovar.insert({name, Gvar});
+                            auto it = Instrtovar.find(&I);
+                            if (it != Instrtovar.end()) {
+                                Instrtovar[&I].insert(Gvar);
+                            }
+                            else {
+                                set<GlobalVar*> s;
+                                s.insert(Gvar);
+                                Instrtovar[&I] = s;
+                            }
                         }
                         if (isa<GEPOperator>(op)) {
                             GEPOperator *GEPOp = dyn_cast<GEPOperator>(op);
@@ -187,6 +208,7 @@ int main(int argc, char *argv[]) {
                             if (auto *PT = dyn_cast<PointerType>(basevalue->getType())) {
                                 string fullname = "";
                                 fullname += basevalue->getName().str();
+                                string base = fullname;
                                 if (auto *AT = dyn_cast<ArrayType>(PT->getElementType())) {
                                     //是数组
                                     Type *ElementTypeOfAT = AT->getElementType();
@@ -261,6 +283,7 @@ int main(int argc, char *argv[]) {
                                     Gvar->setID(id++);
                                     Gvar->setType(2);
                                     Gvar->addBelongtofun(funname);
+                                    Gvar->setBaseName(base);
                                     if (I.getDebugLoc()) {
                                         unsigned line = I.getDebugLoc().getLine();
                                         Gvar->addbelongtoline(line);
@@ -268,6 +291,23 @@ int main(int argc, char *argv[]) {
                                     }
                                     globals.insert(Gvar);
                                     nametovar.insert({fullname, Gvar});
+                                    if (basenametoname.find(base) != basenametoname.end()) {
+                                        basenametoname[base].insert(fullname);
+                                    }
+                                    else {
+                                        set<string> s;
+                                        s.insert(fullname);
+                                        basenametoname[base] = s;
+                                    }
+                                    auto it = Instrtovar.find(&I);
+                                    if (it != Instrtovar.end()) {
+                                        Instrtovar[&I].insert(Gvar);
+                                    }
+                                    else {
+                                        set<GlobalVar*> s;
+                                        s.insert(Gvar);
+                                        Instrtovar[&I] = s;
+                                    }
                                 }
                                 else if (auto *ST = dyn_cast<StructType>(PT->getElementType())) {
                                     //是结构体
@@ -302,6 +342,7 @@ int main(int argc, char *argv[]) {
                                     GlobalVar *Gvar = new GlobalVar(fullname);
                                     Gvar->setID(id++);
                                     Gvar->setType(2);
+                                    Gvar->setBaseName(base);
                                     Gvar->addBelongtofun(funname);
                                     if (I.getDebugLoc()) {
                                         unsigned line = I.getDebugLoc().getLine();
@@ -310,6 +351,23 @@ int main(int argc, char *argv[]) {
                                     }
                                     globals.insert(Gvar);
                                     nametovar.insert({fullname, Gvar});
+                                    if (basenametoname.find(base) != basenametoname.end()) {
+                                        basenametoname[base].insert(fullname);
+                                    }
+                                    else {
+                                        set<string> s;
+                                        s.insert(fullname);
+                                        basenametoname[base] = s;
+                                    }
+                                    auto it = Instrtovar.find(&I);
+                                    if (it != Instrtovar.end()) {
+                                        Instrtovar[&I].insert(Gvar);
+                                    }
+                                    else {
+                                        set<GlobalVar*> s;
+                                        s.insert(Gvar);
+                                        Instrtovar[&I] = s;
+                                    }
                                 }
                             }
                         }
@@ -322,38 +380,88 @@ int main(int argc, char *argv[]) {
     }
 
     //先搜集一下距离近的变量
-    for (auto *f : funinfos) {
-        sort(f->vars.begin(), f->vars.end(), [&] (const pair<string, int> &p1, const pair<string, int> &p2) -> bool {
-            return p1.second < p2.second;
-        });
+//    for (auto *f : funinfos) {
+//        sort(f->vars.begin(), f->vars.end(), [&] (const pair<string, int> &p1, const pair<string, int> &p2) -> bool {
+//            return p1.second < p2.second;
+//        });
+//
+//        for (auto &p : f->vars) {
+//            for (auto &q : f->vars) {
+//                if (p == q)
+//                    continue;
+//                if (q.first == p.first)
+//                    continue;
+//                if (q.second >= p.second - codedistance / 2 && q.second <= p.second + codedistance / 2) {
+//                    //搞一个容器装所有的全局变量名字，再进行搜索
+//                    //map<string, GlobalVar*>
+//                    //添加每个GlobalVar的codeclose
+//                    nametovar[p.first]->addClosedis(q.first);
+//                }
+//            }
+//        }
+//    }
+//    for (auto &i : globals) {
+//        outs() << i->getName() << ":\n";
+//        if (i->getClosedis().empty()) {
+//            continue;
+//        }
+//        for (auto &j : i->getClosedis()) {
+//            outs() << j.first << " " << j.second << " times\n";
+//        }
+//    }
 
-        for (auto &p : f->vars) {
-            for (auto &q : f->vars) {
-                if (p == q)
-                    continue;
-                if (q.first == p.first)
-                    continue;
-                if (q.second >= p.second - codedistance / 2 && q.second <= p.second + codedistance / 2) {
-                    //搞一个容器装所有的全局变量名字，再进行搜索
-                    //map<string, GlobalVar*>
-                    //添加每个GlobalVar的codeclose
-                    nametovar[p.first]->addClosedis(q.first);
+    //搜集具有兄弟关系的变量
+    for (auto &g : globals) {
+        if (g->getType() == 1)
+            continue;
+        auto it = basenametoname.find(g->getBaseName());
+        if (it != basenametoname.end()) {
+            for (auto &bg : it->second) {
+                if (bg == g->getName())
+                    continue ;
+                g->addBrother(bg);
+            }
+        }
+        for (auto &g2 : globals) {
+            string gn = g->getName(), g2n = g2->getName();
+            if (g2n == gn)
+                continue;
+            if (isCloseName(gn, g2n)) {
+                g->addClosename(g2n);
+            }
+        }
+    }
+//    for (auto &g : globals) {
+//        outs() << g->getName() << ": ";
+//        for (auto &gb : g->getBrother()) {
+//            outs() << gb << " ";
+//        }
+//        outs() << "\n";
+//    }
+    //搜集具有相似命名的变量
+    for (auto &g : globals) {
+        outs() << g->getName() << ": ";
+        for (auto &cn : g->getClosename()) {
+            outs() << cn << " ";
+        }
+        outs() << "\n";
+    }
+    //搜集具有依赖关系的变量
+    //先找每个变量对应哪些节点
+    for (auto *dg : sdg.getSDG()) {
+        for (auto *node : dg->getNodes()) {
+            if (Value *v = sdg.getValue(node)) {
+                if (auto *inst = dyn_cast<Instruction>(v)) {
+                    auto it = Instrtovar.find(inst);
+                    if (it != Instrtovar.end()) {
+                        for (auto &g : it->second) {
+                            g->addnode(node);
+                        }
+                    }
                 }
             }
         }
     }
-    for (auto &i : globals) {
-        outs() << i->getName() << ":\n";
-        if (i->getClosedis().empty()) {
-            continue;
-        }
-        for (auto &j : i->getClosedis()) {
-            outs() << j.first << " " << j.second << " times\n";
-        }
-    }
-    //搜集具有兄弟关系的变量
-    //搜集具有相似命名的变量
-    //搜集具有依赖关系的变量
     return 0;
 }
 bool isStructArray(Type* type) {
@@ -365,4 +473,70 @@ bool isStructArray(Type* type) {
         return true;
     }
     return false;
+}
+bool isCloseName(const string &name1, const string &name2) {
+    //ddd[0]还是会匹配到d
+    size_t len1 = name1.size(), len2 = name2.size();
+    size_t minlen, i;
+    if (len1 < len2) {
+        minlen = len1;
+        if (name2.find(name1) == 0) {
+            return true;
+        }
+        for (i = 0; i < minlen; i++) {
+            if (name1[i] != name2[i]) {
+                break;
+            }
+        }
+    }
+    else if (len1 > len2) {
+        minlen = len2;
+        if (name1.find(name2) == 0) {
+            return true;
+        }
+        for (i = 0; i < minlen; i++) {
+            if (name1[i] != name2[i]) {
+                break;
+            }
+        }
+    }
+    else {
+        if (name1 == name2)
+            return true;
+        for (i = 0; i < len1; i++) {
+            if (name1[i] != name2[i]) {
+                break;
+            }
+        }
+
+    }
+    if (i == 0)
+        return false;
+    if (i == minlen)
+        i--;
+    string prefix = name1.substr(0, i);
+    char lastchar = prefix.back();
+    if (lastchar == '[' || lastchar == '.')
+        return true;
+    if (i < minlen && isalpha(lastchar) && (islower(lastchar) && isupper(name1[i + 1]) || isupper(lastchar) && islower(name1[i + 1])))
+        return true;
+    return false;
+}
+void Dep(llvmdg::SystemDependenceGraph &sdg, GlobalVar *Gvar) {
+    for (auto *node : Gvar->getnode()) {
+        controldep(sdg, node);
+        datadep(sdg, node);
+    }
+}
+void controldep(llvmdg::SystemDependenceGraph &sdg, dg::sdg::DGNode *node) {
+    if (!node)
+        return;
+    map<dg::sdg::DGNode*, int> visited;//visited[node] = 1是访问过的
+    set<dg::sdg::DGNode*> tofindcomesfrom;
+    
+}
+void datadep(llvmdg::SystemDependenceGraph &sdg, dg::sdg::DGNode *node) {
+    if (!node)
+        return;
+    map<dg::sdg::DGNode*, int> visited;//visited[node] = 1是访问过的
 }
